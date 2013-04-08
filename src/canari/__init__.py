d9b82af4572a631a028651cdae9527cac84d3f5f
allfro <ndouba@gmail.com>
2013-04-07 21:24:12 -0400

    Bump up the version! Bump up the version!

--- src/canari/__init__.py
+++ src/canari/__init__.py
@@ -3,7 +3,7 @@ __copyright__ = 'Copyright 2012, Canari Project'
 __credits__ = []
 
 __license__ = 'GPL'
-__version__ = '0.8'
+__version__ = '0.9'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
