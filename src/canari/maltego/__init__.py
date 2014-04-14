8d42ee11301a542a6c1e3c6e729b9c0b6551a359
allfro <ndouba@gmail.com>
2014-04-14 12:47:44 -0400

    Added additional maltego-related xml parsing and serialization modules.

--- src/canari/maltego/__init__.py
+++ src/canari/maltego/__init__.py
@@ -5,12 +5,15 @@ __copyright__ = 'Copyright 2012, Canari Project'
 __credits__ = []
 
 __license__ = 'GPL'
-__version__ = '0.1'
+__version__ = '0.2'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
 
 __all__ = [
+    'configuration',
+    'entities',
+    'html',
     'message',
     'utils'
 ]
