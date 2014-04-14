a69d5f4c179dcada0655f12b40dcfb9969ec4e9b
allfro <ndouba@gmail.com>
2014-04-14 12:36:10 -0400

    Migrated command to new commander framework

--- src/canari/commands/banner.py
+++ src/canari/commands/banner.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
-from common import get_commands, cmd_name
-from argparse import ArgumentParser
-from sys import modules
-
-import canari
+from common import canari_main
+from framework import SubCommand
 
 
 __author__ = 'Nadeem Douba'
@@ -12,33 +9,24 @@ __copyright__ = 'Copyright 2012, Canari Project'
 __credits__ = []
 
 __license__ = 'GPL'
-__version__ = '0.2'
+__version__ = '0.3'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
 
 
-parser = ArgumentParser(
-    description='Show banner of Canari framework that is currently active.',
-    usage='canari %s' % cmd_name(__name__)
+@SubCommand(
+    canari_main,
+    help='Show banner of Canari framework that is currently active.',
+    description='Show banner of Canari framework that is currently active.'
 )
-
-
-def help_():
-    parser.print_help()
-
-
-def description():
-    return parser.description
-
-
-def run(args):
+def banner(args):
     print """
     Your running ...
- ____ ____ ____ ____ ____ ____ _________ ____ ____ ____
-||c |||a |||n |||a |||r |||i |||       |||1 |||. |||0 ||
-||__|||__|||__|||__|||__|||__|||_______|||__|||__|||__||
-|/__\|/__\|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|
-
-                                ... http://canariproject.com
+        dBBBP dBBBBBb     dBBBBb dBBBBBb   dBBBBBb    dBP    dBBBb   dBBBb
+                   BB        dBP      BB       dBP
+      dBP      dBP BB   dBP dBP   dBP BB   dBBBBK   dBP       dBP     dBP
+     dBP      dBP  BB  dBP dBP   dBP  BB  dBP  BB  dBP       dBP     dBP
+    dBBBBP   dBBBBBBB dBP dBP   dBBBBBBB dBP  dB' dBP       dBP dBP dBP
+                                            ... http://canariproject.com
     """
