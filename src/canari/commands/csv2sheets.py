2d9a516ec7853faccc0ebd05c166648893a81cfb
allfro <ndouba@gmail.com>
2014-04-14 12:40:36 -0400

    Migrated command to the new command framework.
    Fixed ordering of CSV headers.

--- src/canari/commands/csv2sheets.py
+++ src/canari/commands/csv2sheets.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+from collections import OrderedDict
 
-from common import cmd_name
+from framework import Argument, SubCommand
+from common import canari_main
 
 from csv import reader, DictWriter
-from argparse import ArgumentParser
 
 
 __author__ = 'Nadeem Douba'
@@ -11,46 +12,34 @@ __copyright__ = 'Copyright 2012, Canari Project'
 __credits__ = []
 
 __license__ = 'GPL'
-__version__ = '0.3'
+__version__ = '0.4'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
 
 
-parser = ArgumentParser(
-    description='Convert mixed entity type CSVs to separated CSV sheets.',
-    usage='canari %s <graph csv> [sheet prefix]' % cmd_name(__name__)
+@SubCommand(
+    canari_main,
+    help='Convert mixed entity type CSVs to separated CSV sheets.',
+    description='Convert mixed entity type CSVs to separated CSV sheets.'
 )
-
-parser.add_argument(
+@Argument(
     'graph',
     metavar='<graph csv>',
     help='The CSV file containing the output from the mtgx2csv command.'
 )
-
-parser.add_argument(
+@Argument(
     'prefix',
     metavar='[sheet prefix]',
     nargs='?',
     help='The prefix to prepend to the generated CSV files.'
 )
+def csv2sheets(opts):
 
-
-def help_():
-    parser.print_help()
-
-
-def description():
-    return parser.description
-
-
-def parse_args(args):
-    return parser.parse_args(args)
-
-
-def run(args):
-
-    opts = parse_args(args)
+    if not opts.graph.endswith('.csv'):
+        print "Invalid file type: %s. Please make sure you run this command against a CSV " \
+              "file generated from 'canari mtgx2csv'." % opts.graph
+        exit(-1)
     opts.prefix = opts.prefix or opts.graph.split('.', 1)[0]
 
     sheets = {}
@@ -59,20 +48,22 @@ def run(args):
     try:
         with file(opts.graph) as csvfile:
             for row in reader(csvfile):
-                fv = dict(column.split('=', 1) for column in row)
+                fv = OrderedDict(column.split('=', 1) for column in row)
                 entity_type = fv.pop('Entity Type')
                 headers = fv.keys()
                 if entity_type not in sheets:
                     sheets[entity_type] = [fv]
-                    sheet_headers[entity_type] = set(headers)
+                    sheet_headers[entity_type] = headers
                     continue
                 else:
                     sheets[entity_type].append(fv)
                 if len(headers) > len(sheet_headers[entity_type]):
-                    sheet_headers[entity_type].union(headers)
+                    sheet_headers[entity_type].extend([h for h in headers if h not in sheet_headers[entity_type]])
 
         for entity_type in sheets:
-            with open('%s_%s.csv' % (opts.prefix, entity_type), 'wb') as csvfile:
+            filename = '%s_%s.csv' % (opts.prefix, entity_type)
+            print 'Writing %s sheet to %s...' % (entity_type, filename)
+            with open(filename, 'wb') as csvfile:
                 csv = DictWriter(csvfile, sheet_headers[entity_type])
                 csv.writeheader()
                 csv.writerows(sheets[entity_type])
