e1745fe3967cc835f1c9e510b7bbeb3a80015277
allfro <ndouba@gmail.com>
2014-04-14 12:45:16 -0400

    Migrated command to new command framework.

--- src/canari/commands/version.py
+++ src/canari/commands/version.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-from common import get_commands, cmd_name
-from argparse import ArgumentParser
-from sys import modules
+from common import canari_main
+from framework import SubCommand
 
 import canari
 
@@ -12,25 +11,16 @@ __copyright__ = 'Copyright 2012, Canari Project'
 __credits__ = []
 
 __license__ = 'GPL'
-__version__ = '0.2'
+__version__ = '0.3'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
 
 
-parser = ArgumentParser(
-    description='Show version of Canari framework that is currently active.',
-    usage='canari %s' % cmd_name(__name__)
+@SubCommand(
+    canari_main,
+    help='Show version of Canari framework that is currently active.',
+    description='Show version of Canari framework that is currently active.'
 )
-
-
-def help_():
-    parser.print_help()
-
-
-def description():
-    return parser.description
-
-
-def run(args):
+def version(args):
     print 'Your running *Canari Framework v%s*' % canari.__version__
