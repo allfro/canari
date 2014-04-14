2b67c8f68e6f6cf7918ab8a81c37e10551df8979
allfro <ndouba@gmail.com>
2014-04-14 13:46:16 -0400

    Migrated command to new command framework.
    Added additional error checking.

--- src/canari/commands/create_transform.py
+++ src/canari/commands/create_transform.py
@@ -1,63 +1,53 @@
 #!/usr/bin/env python
 
-from common import write_template, read_template, cmd_name, init_pkg, project_tree
+import os
+import re
 
-from argparse import ArgumentParser
-from os import path
-from re import sub
-
+from common import write_template, read_template, canari_main, init_pkg, project_tree
+from framework import SubCommand, Argument
 
 __author__ = 'Nadeem Douba'
 __copyright__ = 'Copyright 2012, Canari Project'
 __credits__ = []
 
 __license__ = 'GPL'
-__version__ = '0.3'
+__version__ = '0.4'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
 
 
-parser = ArgumentParser(
-    description='Creates a new transform in the specified directory and auto-updates __init__.py.',
-    usage='canari %s <transform name> [options]' % cmd_name(__name__)
-)
+def parse_args(args):
+    if args.transform_dir is None:
+        args.transform_dir = project_tree()['transforms']
+    if args.transform in ['common', 'common.py']:
+        print "Error: 'common' is a reserved module. Please name your transform something else."
+        exit(-1)
+    return args
 
-parser.add_argument(
+
+@SubCommand(
+    canari_main,
+    help='Creates a new transform in the specified directory and auto-updates __init__.py.',
+    description='Creates a new transform in the specified directory and auto-updates __init__.py.'
+)
+@Argument(
     'transform',
     metavar='<transform name>',
     help='The name of the transform you wish to create.'
 )
-
-parser.add_argument(
+@Argument(
     '-d',
     '--transform-dir',
     metavar='<dir>',
     help='The directory in which you wish to create the transform.',
     default=None
 )
-
-
-def help_():
-    parser.print_help()
-
-
-def description():
-    return parser.description
-
-
-def parse_args(args):
-    args = parser.parse_args(args)
-    if args.transform_dir is None:
-        args.transform_dir= project_tree()['transforms']
-    return args
-
-
-def run(args):
+def create_transform(args):
 
     opts = parse_args(args)
 
-    initf = path.join(opts.transform_dir, '__init__.py')
+    initf = os.path.join(opts.transform_dir, '__init__.py')
     transform = opts.transform if not opts.transform.endswith('.py') else opts.transform[:-3]
 
     if '.' in transform:
@@ -68,12 +58,12 @@ def run(args):
         exit(-1)
 
     directory = opts.transform_dir
-    transformf = path.join(directory, opts.transform if opts.transform.endswith('.py') else '%s.py' % opts.transform )
+    transformf = os.path.join(directory, opts.transform if opts.transform.endswith('.py') else '%s.py' % opts.transform )
 
-    if not path.exists(initf):
+    if not os.path.exists(initf):
         print ('Directory %s does not appear to be a python package directory... quitting!' % repr(opts.transform_dir))
         exit(-1)
-    if path.exists(transformf):
+    if os.path.exists(transformf):
         print ('Transform %s already exists... quitting' % repr(transformf))
         exit(-1)
 
@@ -89,7 +79,7 @@ def run(args):
 
     with file(initf, mode='wb') as w:
         w.write(
-            sub(
+            re.sub(
                 r'__all__\s*\=\s*\[',
                 '__all__ = [\n    %s,' % repr(transform),
                 init
