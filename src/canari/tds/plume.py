6ad48453b9c90e8c76ae93329efa2486ee366f82
allfro <ndouba@gmail.com>
2014-04-14 22:48:54 -0400

    Migrated plume to safedexml implementations and cleaned up code.

--- src/canari/tds/plume.py
+++ src/canari/tds/plume.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
 
 # Builtin imports
-from xml.etree.cElementTree import fromstring
 from ConfigParser import NoSectionError
-from cStringIO import StringIO
 from urlparse import urljoin
-import importlib
 import hashlib
 import sys
 import os
 import re
 
 # Third-party imports
+from canari.pkgutils.transform import TransformDistribution
 from flask import Flask, Response, request
 
 # Canari imports
 import canari.config as _config
-from canari.commands.common import get_transform_version
-from canari.maltego.message import (Message, MaltegoMessage, MaltegoTransformRequestMessage,
-                                    MaltegoTransformResponseMessage, MaltegoTransformExceptionMessage, MaltegoException)
+from canari.maltego.utils import get_transform_version
+from canari.maltego.message import (MaltegoMessage, MaltegoTransformResponseMessage, MaltegoTransformExceptionMessage,
+                                    MaltegoException)
 from canari.resource import image_resources
 
 
@@ -27,13 +25,14 @@ __copyright__ = 'Copyright 2012, Canari Project'
 __credits__ = []
 
 __license__ = 'GPL'
-__version__ = '0.3'
+__version__ = '0.4'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
 
 
 class Canari(Flask):
+
     def __init__(self, import_name):
         super(Canari, self).__init__(import_name)
         self.transforms = {}
@@ -45,7 +44,7 @@ class Canari(Flask):
         for i in image_resources(pkg):
             dname = 'static/%s' % hashlib.md5(i).hexdigest()
             if not os.path.exists(dname):
-                with file(i, mode='rb') as src:
+                with open(i, mode='rb') as src:
                     with open(dname, mode="wb") as dst:
                         dst.write(src.read())
 
@@ -79,44 +78,34 @@ class Canari(Flask):
             # Copy all the image resource files in case they are used as entity icons
             self._copy_images(p)
 
-            if not p.endswith('.transforms'):
-                p = ('%s.transforms' % p)
+            distribution = TransformDistribution(p)
 
             sys.stderr.write('Loading transform package %s\n' % repr(p))
 
-            # Load our first transform package
-            m = importlib.import_module(p)
-
-            for transform_name in m.__all__:
-                transform_name = ('%s.%s' % (p, transform_name))
-
-                # Let's import our transforms one by one
-                transform_module = importlib.import_module(transform_name)
-                if not hasattr(transform_module, 'dotransform'):
+            for transform in distribution.remote_transforms:
+                transform_name = transform.__name__
+                sys.stderr.write('Loading transform %s at /%s...\n' % (repr(transform_name), transform_name))
+                # Should the transform be publicly available?
+                # Does it conform to V2 of the Canari transform signature standard?
+                if get_transform_version(transform.dotransform) == 2:
+                    sys.stderr.write('ERROR: Plume does not support V2 Canari transforms (%s). Please update to V3.'
+                                     ' See http://www.canariproject.com/4-3-transform-development-quick-start/ for'
+                                     ' more details.\n' % repr(transform_name))
+                    exit(-1)
+                    # Does the transform need to be executed as root? If so, is this running in mod_wsgi? Yes = Bad!
+                elif os.name == 'posix' and hasattr(transform.dotransform, 'privileged') and \
+                        os.geteuid() and __name__.startswith('_mod_wsgi_'):
+                    sys.stderr.write('WARNING: mod_wsgi does not allow applications to run with root privileges. '
+                                     'Transform %s ignored...\n' % repr(transform_name))
                     continue
 
-                # Should the transform be publicly available?
-                if hasattr(transform_module.dotransform, 'remote') and transform_module.dotransform.remote:
-                    sys.stderr.write('Loading transform %s at /%s...\n' % (repr(transform_name), transform_name))
-                    # Does it conform to V2 of the Canari transform signature standard?
-                    if get_transform_version(transform_module.dotransform) == 2:
-                        sys.stderr.write('ERROR: Plume does not support V2 Canari transforms (%s). Please update to V3.'
-                                         ' See http://www.canariproject.com/4-3-transform-development-quick-start/ for'
-                                         ' more details.\n' % repr(transform_name))
-                        exit(-1)
-                    # Does the transform need to be executed as root? If so, is this running in mod_wsgi? Yes = Bad!
-                    elif os.name == 'posix' and hasattr(transform_module.dotransform, 'privileged') and \
-                            os.geteuid() and __name__.startswith('_mod_wsgi_'):
-                        sys.stderr.write('WARNING: mod_wsgi does not allow applications to run with root privileges. '
-                                         'Transform %s ignored...\n' % repr(transform_name))
-                        continue
-                    # So everything is good, let's register our transform with the global transform registry.
-                    inputs = {}
-                    if hasattr(transform_module.dotransform, 'inputs'):
-                        for category, entity_type in transform_module.dotransform.inputs:
-                            inputs[entity_type._type_] = entity_type
-                            inputs[entity_type._v2type_] = entity_type
-                    self.transforms[transform_name] = (transform_module.dotransform, inputs)
+                # So everything is good, let's register our transform with the global transform registry.
+                inputs = {}
+                if hasattr(transform.dotransform, 'inputs'):
+                    for category, entity_type in transform.dotransform.inputs:
+                        inputs[entity_type._type_] = entity_type
+                        inputs[entity_type._v2type_] = entity_type
+                self.transforms[transform_name] = (transform.dotransform, inputs)
 
 
 # Create our Flask app.
@@ -125,13 +114,13 @@ app = Canari(__name__)
 
 def croak(error_msg):
     """Throw an exception in the Maltego GUI containing error_msg."""
-    s = StringIO()
-    Message(
-        MaltegoMessage(
-            MaltegoTransformExceptionMessage(exceptions=MaltegoException(error_msg))
+    return MaltegoMessage(
+        message=MaltegoTransformExceptionMessage(
+            exceptions=[
+                MaltegoException(error_msg)
+            ]
         )
-    ).write(file_or_filename=s)
-    return s.getvalue()
+    ).render()
 
 
 def message(m):
@@ -145,7 +134,6 @@ def message(m):
             m.replace(url, new_url, 1)
         v = m
     else:
-        sio = StringIO()
         # Let's make sure that we're not spewing out local file system information ;)
         for e in m.entities:
             if e.iconurl is not None:
@@ -154,11 +142,9 @@ def message(m):
                     path = 'static/%s' % hashlib.md5(e.iconurl[7:]).hexdigest()
                     new_url = urljoin(request.host_url, path)
                     e.iconurl = new_url
-
-        Message(MaltegoMessage(m)).write(sio)
-        v = sio.getvalue()
+        v = MaltegoMessage(message=m).render()
     # Get rid of those nasty unicode 32 characters
-    return Response(re.sub(r'(&#\d{5};){2}', r'', v), status=200, mimetype='text/html')
+    return Response(v, status=200, mimetype='text/html')
 
 
 def dotransform(transform, valid_input_entity_types):
@@ -167,34 +153,28 @@ def dotransform(transform, valid_input_entity_types):
         request_str = request.data
 
         # Let's get an XML object tree
-        xml = fromstring(request_str).find('MaltegoTransformRequestMessage')
+        maltego_request = MaltegoMessage.parse(request_str).message
 
         # Get the entity being passed in.
-        e = xml.find('Entities/Entity')
-        entity_type = e.get('Type', '')
+        e = maltego_request.entity
+        entity_type = e.type
 
         if valid_input_entity_types and entity_type not in valid_input_entity_types:
             return Response(status=404)
 
-        # Initialize Maltego Request values to pass into transform
-        value = e.find('Value').text or ''
-        fields = dict([(f.get('Name', ''), f.text) for f in xml.findall('Entities/Entity/AdditionalFields/Field')])
-        params = dict([(f.get('Name', ''), f.text) for f in xml.findall('TransformFields/Field')])
-        limits = xml.find('Limits').attrib
-
         # Initialize a private copy of the config to pass into the transform
         config = _config.CanariConfigParser()
-        for k, i in params.items():
-            if '.' in k:
-                config[k.replace('.', '/', 1)] = i
+        for p in maltego_request.parameters.values():
+            if '.' in p.name:
+                config[p.name.replace('.', '/', 1)] = p.value
             else:
-                config['default/%s' % k] = i
+                config['plume/%s' % p.name] = p.value
         # The private config variables CANNOT override the server's settings. This is for security?
         config._sections.update(_config.config._sections)
 
         # Execute it!
         msg = transform(
-            MaltegoTransformRequestMessage(value, fields, params, limits),
+            maltego_request,
             request_str if hasattr(transform, 'cmd') and callable(transform.cmd) else MaltegoTransformResponseMessage(),
             config
         )
